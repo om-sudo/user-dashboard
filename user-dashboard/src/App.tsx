1f0ba6cb8a58697fe4dd47303501574a0cb6a169
om <omthummar260802@gmail.com>
2024-02-26 23:58:07 +0530

    changes

--- user-dashboard/src/App.tsx
+++ user-dashboard/src/App.tsx
@@ -1,23 +1,6 @@
 import Page from "./users/Page"
 
 function App() {
-
-  const currentDate = new Date();
-
-  const options = {
-    year: 'numeric',
-    month: 'numeric',
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    hour12: false
-  };
-  
-  const formattedDateTime = new Intl.DateTimeFormat('en-US', options).format(currentDate);
-  
-  console.log(formattedDateTime);
-  
   return (
     <>
       <Page />
